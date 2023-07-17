0e0fe38c40372dda27f952df1855d48b5ef9b333
Breno Silva <brennohsilva@hotmail.com>
2023-07-17 20:52:54 -0300

    add favicon and backend url

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ export default function Document() {
     return (
         <Html>
             <Head>
-
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png"/>
+                <link rel="android-icon" type="image/png" sizes="192x192"  href="/android-chrome-192x192.png"/>
+                <link rel="android-icon" type="image/png" sizes="512x512"  href="/android-chrome-512x512.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                
             </Head>
             <body className='bg-sitebg bg-cover p-4'>
                 <Main/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home(props: HomeProps) {
         
             setTimeout(() =>{
                 router.push('/results')
-            }, 4000)
+            }, 3000)
 
         } catch(err){
             console.log(err)
